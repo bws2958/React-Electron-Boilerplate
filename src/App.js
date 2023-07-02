ee608b50ea89375ea2605b38f3b85d036b3fd7fc
HMS <bws2958@gmail.com>
2023-07-02 10:08:31 +0900

    2nd commit.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
@@ -18,6 +16,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>
+        Hello world2
+      </h1>
     </div>
   );
 }
