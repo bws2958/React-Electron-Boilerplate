459ed714d4e25a3cbf3eaab7054e7245a915afa8
HMS <bws2958@gmail.com>
2023-07-16 10:29:11 +0900

    230716 1st commit - react electron success

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
         
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>
-        Hello world2
-      </h1>
+//         <p>
+//           Hello world!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <h1>
+//         Hello world2
+//       </h1>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+const App = () => {
+  const movePage = (page) => {
+    window.location.href = page;
+  };
+
+  return (
+    <div style={{ backgroundColor: '#c2c2d6' }}>
+      <input
+        type="button"
+        className="button button1"
+        value="Create new account"
+        onClick={() => movePage('New_account')}
+      />
+      <input
+        type="button"
+        className="button button1"
+        value="Log in"
+        onClick={() => movePage('Login_try')}
+      />
     </div>
   );
-}
+};
+
+
 
 export default App;
