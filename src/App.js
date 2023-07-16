be589cbb0725cebaf99ed79b691e2b844e64e988
HMS <bws2958@gmail.com>
2023-07-16 11:04:22 +0900

    230716 4th commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-        
-//         <p>
-//           Hello world!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//       <h1>
-//         Hello world2
-//       </h1>
-//     </div>
-//   );
-// }
-
 import React from 'react';
 
 const App = () => {
